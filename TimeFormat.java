f6a62adeba203cd0faf6940baf95ba3b9a1eb6f3
shirgnapp <gnapp3@gmail.com>
2024-11-11 21:11:58 +0200

    completed

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,17 +1,51 @@
 // Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
 	public static void main(String[] args) {
-		// In Java, the command-line arguments args[0], args[1], ... are represented
-		// each by a string. In this program, the single "hh:mm" input is represented
-		// by the single command-line string argument args[0]. 
-		//   
-		// The following statement handles the hours part of the input.
-		// It concatenates the empty string "" with the leftmost hour-digit. 
-		// It then concatenates the resulting string with the rightmost hour-digit,
-		// and then uses parseInt to cast the resulting string as an int.
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		// Does the same with the minutes part of the input.
-		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        if (hours >  12)
+        {
+            if (hours==13) {
+                System.out.print("01:");
+            }
+            if (hours==14) {
+                System.out.print("02:");  
+            }
+            if (hours==15) {
+                System.out.print("03:");
+            }
+            if (hours==16) {
+                System.out.print("04:");
+            }
+            if ((hours==17)) {
+                System.out.print("05:");
+            }
+            if (hours==18) {
+                System.out.print("06:");
+            }
+            if (hours==19) {
+                System.out.print("07:");
+            }
+            if (hours==20) {
+                System.out.print("08:");
+            }
+           if (hours==21) {
+                System.out.print("09:");
+           }
+           if (hours==22) {
+                System.out.print("10:");
+           }
+           if (hours==23) {
+                System.out.print("11:");
+           }
+           if (hours==24) {
+               System.out.print("12:");
+           }
+           System.out.println(minutes+ " PM");
+        }
+        else
+           {
+               System.out.print("0"+hours+":" + minutes+ " AM");
+           }
 	}
 }
