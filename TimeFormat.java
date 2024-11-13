dcb6de8f69c66f1331e72760105acfe978ff8a2a
shirgnapp <gnapp3@gmail.com>
2024-11-13 15:13:57 +0200

    fixed again

--- TimeFormat.java
+++ TimeFormat.java
@@ -5,32 +5,32 @@ public class TimeFormat {
         int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         if (hours > 12) {
             if (minutes<10) {
-                System.out.println("0"+ (hours-12)+":0"+minutes+"PM");
+                System.out.println("0"+ (hours-12)+":0"+minutes+" PM");
             }
             else
-            System.out.println("0"+ (hours-12)+":"+minutes+"PM");
+            System.out.println("0"+ (hours-12)+":"+minutes+" PM");
         }
         if (hours < 12 && hours!=0) 
 		{
             if (minutes<10) {
-                System.out.println("0"+hours+":0"+minutes+"AM");
+                System.out.println("0"+hours+":0"+minutes+" AM");
             }
             else
-            System.out.println("0"+hours+":"+minutes+"AM");
+            System.out.println("0"+hours+":"+minutes+" AM");
         }
 		if (hours==12){
 			if (minutes<10) {
-			System.out.println("12:0"+minutes+"PM");
+			System.out.println("12:0"+minutes+" PM");
 			}
 			else 
-			System.out.println("12:"+minutes+"PM");
+			System.out.println("12:"+minutes+" PM");
 		}
 		if (hours==0) {
 			if (minutes<10) {
-				System.out.println("12:0"+minutes+"AM");
+				System.out.println("12:0"+minutes+" AM");
 				}
 				else 
-				System.out.println("12:"+minutes+"AM");
+				System.out.println("12:"+minutes+" AM");
 		}
 	}
 }
